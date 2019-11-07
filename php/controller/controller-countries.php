61c90279680a02fca5c58c087bb16dc764729135
Jeremias ramirez <jereramirez499@gmail.com>
2019-11-07 19:44:29 -0400

    mejorando controlador de backend

--- php/controller/controller-countries.php
+++ php/controller/controller-countries.php
@@ -2,6 +2,12 @@
 
 include "../model/countries.php";
 
+$numPagination  = 0;
+// $numPagination = $_GET[]
+
+if(isset($_GET["numpagination"])){
+    $numPagination = $_GET["numpagination"];
+}
 
 $country = new countries();
-$country->get_countries(0);
+$country->get_countries($numPagination);
